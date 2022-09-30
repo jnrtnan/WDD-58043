b3f64512aacd2db1d842f1239e9b1cc7ec85b402
Jan Retanan <90012772+jnrtnan@users.noreply.github.com>
2022-09-30 09:42:29 +0800

    Add files via upload

---
+++ Exercise1.php
@@ -0,0 +1,24 @@
+<html>
+<head><title>Exercise 1</title><head>
+<body bgcolor = "#FFE5B4">
+<?php 
+$FullName = "Jan Lemuel C. Retanan";
+$CollgeProgram  = "Computer Engineering";
+$YandS = "3rd Year WDD-58043";
+$string = "Today, I will learn PHP programming";
+
+echo "<h1> Exercise 1</h1>";
+echo "<b>Name: </b>". $FullName;
+echo "<br>";
+echo "<b>Collge Program: </b>". $CollgeProgram;
+echo "<br>";
+echo "<b>Year and Section: </b>". $YandS;
+echo "<br>";
+echo "<h3 align='center'>$string</h3>";
+
+
+
+
+
+?>
+</html>
